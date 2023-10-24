2c77e2b7ec6d764d5a234a86610d5b55e90c9140
Ross King <ross.king@altavec.com>
2023-10-24 15:17:33 +1100

    🎉 assemblies to help with commandline parsing

---
+++ src/CommandLine/Invocation/INestedAction{T}.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="INestedAction{T}.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine.Invocation;
+
+/// <summary>
+/// A nested <see cref="CliAction"/>.
+/// </summary>
+/// <typeparam name="T">The action type.</typeparam>
+public interface INestedAction<out T>
+    where T : CliAction
+{
+    /// <summary>
+    /// Gets the action.
+    /// </summary>
+    T Action { get; }
+}
---
+++ src/CommandLine/Invocation/NestedSynchronousCliAction.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NestedSynchronousCliAction.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine.Invocation;
+
+/// <summary>
+/// A nested <see cref="SynchronousCliAction"/>.
+/// </summary>
+/// <remarks>
+/// Initialises a new instance of the <see cref="NestedSynchronousCliAction"/> class.
+/// </remarks>
+/// <param name="action">The <see cref="CliAction"/>.</param>
+public class NestedSynchronousCliAction(SynchronousCliAction action) : SynchronousCliAction, INestedAction<SynchronousCliAction>
+{
+    /// <inheritdoc/>
+    public SynchronousCliAction Action { get; } = action;
+
+    /// <inheritdoc/>
+    public override int Invoke(ParseResult parseResult) => this.Action.Invoke(parseResult);
+}
